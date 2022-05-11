c9c9d1e7b8bcf37935de676be601427d92b69558
Pavel <pavelsuspicin@gmail.com>
2022-05-11 10:53:05 +0300

    11.05.22

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
 
+toggles.forEach((toggle) => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(clicked) {
+    if (good.checked && cheap.checked & fast.checked) {
+        if (good === clicked) {
+            cheap.checked = false
+        }
+        if (cheap === clicked) {
+            fast.checked = false
+        }
+        if (fast === clicked) {
+            good.checked = false
+        }
+    }
+}
